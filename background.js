30436907785d4ba55488e8ed1e7e118125e64ee9
jaap vstr <password-derivative@eenblauwehemel.nl>
2022-01-19 15:35:13 +0100

    Argon2 implementation had bugs: discontinued Argon2 as it turns out to be platform dependend

--- background.js
+++ background.js
@@ -109,21 +109,6 @@ function connected(p) {
 								var finalBase64String = base64String.slice(0, passwordsize);
 								continueEncryptPassword(finalBase64String, hostname, complexdomains);
 								break;
-							  case "argon2d-time1-parallel1-mem32-hashLen33":
-								argon2.hash({ pass: passwordvalue, salt: hostname, time: 1, parallelism: 1, mem: 32, hashLen: 33, type: argon2.ArgonType.Argon2d})
-								.then(h => continueEncryptPassword(h.encoded.substring(h.encoded.length - passwordsize), hostname, complexdomains))
-								.catch(e => console.error(e.message, e.code));
-								break;
-							  case "argon2i-time1-parallel1-mem32-hashLen33":
-								argon2.hash({ pass: passwordvalue, salt: hostname, time: 1, parallelism: 1, mem: 32, hashLen: 33, type: argon2.ArgonType.Argon2i})
-								.then(h => continueEncryptPassword(h.encoded.substring(h.encoded.length - passwordsize), hostname, complexdomains))
-								.catch(e => console.error(e.message, e.code));
-								break;
-							  case "argon2id-time1-parallel1-mem32-hashLen33":
-								argon2.hash({ pass: passwordvalue, salt: hostname, time: 1, parallelism: 1, mem: 32, hashLen: 33, type: argon2.ArgonType.Argon2id})
-								.then(h => continueEncryptPassword(h.encoded.substring(h.encoded.length - passwordsize), hostname, complexdomains))
-								.catch(e => console.error(e.message, e.code));
-								break;
 							  default:
 								var newHMAC = CryptoJS.HmacSHA512(hostname, passwordvalue);
 								var base64String = CryptoJS.enc.Base64.stringify(newHMAC);
